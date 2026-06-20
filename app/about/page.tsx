b13c7fa39a5e378fb984fdb1c30f26de02f8530d
tsr0705 <tanmaysingh8246@gmail.com>
2026-06-20 22:39:35 +0530

    fixed the about section spacing

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ import AboutSection from "@/components/about-section";
 
 export default function AboutPage() {
   return (
-    <main className="min-h-screen lg:h-screen lg:max-h-screen lg:overflow-hidden w-full bg-black pt-20 lg:pt-0 flex flex-col justify-end">
+    <main className="min-h-screen w-full bg-black pt-28 md:pt-36 flex flex-col justify-center pb-12 md:pb-20">
       <AboutSection />
     </main>
   );
